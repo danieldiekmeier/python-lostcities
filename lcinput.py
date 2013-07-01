c82effe272a1f94389401e35a22de1559b2222c6
Daniel Diekmeier <danieldiekmeier@gmail.com>
2013-07-01 14:26:47 +0200

    Added drawing of cards
    Signed-off-by: Daniel Diekmeier <danieldiekmeier@gmail.com>

--- lcinput.py
+++ lcinput.py
@@ -16,4 +16,12 @@ class Player():
 		return {'location': int(location), 'color': str(color), 'number': int(number)}
 
 	def draw(self, status):
-		return {'location': 0, 'color': 'red'}
+		print "Where to draw card from?\n0: Deck / 1: Discard"
+		location = None
+		color = None
+		while location != 0 and location != 1:
+			location = int(raw_input("Location: "))
+		if location == 1:
+			while color not in status['colors']:
+				color = raw_input("Color: ")
+		return {'location': location, 'color': color}
