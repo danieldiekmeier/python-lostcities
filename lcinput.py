b8f7d2a822510c6beb75f7a9a9808054fc197f20
Daniel Diekmeier <danieldiekmeier@gmail.com>
2013-07-01 11:24:58 +0200

    Added module for manual input
    Signed-off-by: Daniel Diekmeier <danieldiekmeier@gmail.com>

--- lcinput.py
+++ lcinput.py
@@ -1,2 +1,13 @@
-def play():
-	return True
+class Player():
+	def __init__(self):
+		return None
+	def put(self, status):
+		print status['cards']
+		print "Where to put card?\n0: Board\n1: Discard"
+		location = raw_input("Location: ")
+		color = raw_input("Color: ")
+		number = raw_input("Number: ")
+		return {'location': int(location), 'color': str(color), 'number': int(number)}
+
+	def draw(self, status):
+		return {'location': 0, 'color': 'red'}
